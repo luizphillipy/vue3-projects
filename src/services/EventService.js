1149dd45faf89facdcff425d74b0590c375e1b34
Luiz Phillipy Moreira Sampaio <98000497+luizphillipy@users.noreply.github.com>
2022-11-22 13:40:59 -0500

    corection on https

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://my-json-server.typicode.com/luizphillipy/vue3-projects',
+  baseURL: 'https://my-json-server.typicode.com/luizphillipy/vue3-projects',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
